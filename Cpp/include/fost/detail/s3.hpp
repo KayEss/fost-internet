1782103d9ca972a484bc09b8cda255f8a991da3d
Kirit Sælensminde <k@kirit.com>
2009-08-07 06:17:48 +0000

    Added skeleton for new s3put command.

---
+++ Cpp/include/fost/detail/s3.hpp
@@ -0,0 +1,28 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#ifndef FOST_DETAIL_S3_HPP
+#define FOST_DETAIL_S3_HPP
+#pragma once
+
+
+#include <fost/internet>
+#include "http.useragent.hpp"
+
+
+namespace fostlib {
+
+
+    namespace aws {
+    }
+
+
+}
+
+
+#endif // FOST_DETAIL_S3_HPP
