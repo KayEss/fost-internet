bbdf70a079ad0191fd83fb115418a59e899675e9
Kirit Sælensminde <k@kirit.com>
2009-08-10 04:19:25 +0000

    Started to fill in a bit more of the skeleton for s3put.

--- Examples/http/s3put.cpp
+++ Examples/http/s3put.cpp
@@ -18,5 +18,11 @@ FSL_MAIN(
     L"fget",
     L"Amazon S3 client -- push to S3\nCopyright (C) 2008-2009, Felspar Co. Ltd."
 )( fostlib::ostream &o, fostlib::arguments &args ) {
+    if ( args.size() < 3 ) {
+        o << L"Must supply at least one file and a S3 location to put it to" << std::endl;
+        return 1;
+    }
+    url base(args[2].value());
+    o << L"Base URL: " << base << std::endl;
     return 0;
 }
