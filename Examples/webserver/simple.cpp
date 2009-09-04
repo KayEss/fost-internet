8c39f84708be1e30afa93c94f9d64afdf3af2457
Kirit Sælensminde <k@kirit.com>
2009-09-04 10:25:06 +0000

    Brought in the changes to the build mechanisms so that now all libraries are explicitly installed.

--- Examples/webserver/simple.cpp
+++ Examples/webserver/simple.cpp
@@ -24,15 +24,16 @@ namespace {
 FSL_MAIN(
     L"http-simple",
     L"Simple HTTP server\nCopyright (c) 2008-2009, Felspar Co. Ltd."
-)( fostlib::ostream &o, fostlib::arguments & ) {
-    http::server server( host( c_host.value() ), c_port.value() );
+)( fostlib::ostream &o, fostlib::arguments &args ) {
+    http::server server( host( args[1].value(c_host.value()) ), c_port.value() );
     o << L"Answering requests on http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
     for ( bool process( true ); process; ) {
         std::auto_ptr< http::server::request > req( server() );
         o << req->method() << L" " << req->file_spec() << std::endl;
         (*req)( text_body( L"<html><body>This <b>is</b> a response</body></html>", mime::mime_headers(), L"text/html" ) );
-        process = false;
-    };
+        if ( req->data().headers()[L"Host"].value() == L"localhost" )
+            process = false;
+    }
     o << L"Told to exit" << std::endl;
     return 0;
 }
