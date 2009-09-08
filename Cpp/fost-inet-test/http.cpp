c7781456410dbd45168ba2fc2d7aaa14e3f51964
Kirit Sælensminde <k@kirit.com>
2009-09-08 15:30:08 +0000

    HTTP web server requests are now mockable. They can be made in such a way that web server components can be tested.

---
+++ Cpp/fost-inet-test/http.cpp
@@ -0,0 +1,22 @@
+/*
+    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-inet-test.hpp"
+#include <fost/detail/http.server.hpp>
+
+
+using namespace fostlib;
+
+
+FSL_TEST_SUITE( http_server );
+
+
+FSL_TEST_FUNCTION( mock ) {
+    http::server::request req("GET", url::filepath_string("/"), std::auto_ptr< mime >( new empty_mime ));
+    FSL_CHECK_EXCEPTION(req( empty_mime() ), exceptions::null&);
+}
