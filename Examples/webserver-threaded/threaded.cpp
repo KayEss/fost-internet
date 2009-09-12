7781ff65d008b3f50271445c5840ec8128dc7a90
Kirit Sælensminde <k@kirit.com>
2009-09-12 06:27:41 +0000

    Added a simple multi-threaded web server as an example.

---
+++ Examples/webserver-threaded/threaded.cpp
@@ -0,0 +1,45 @@
+/*
+    Copyright 2008-2009 Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/main>
+#include <fost/internet>
+#include <fost/detail/http.server.hpp>
+#include <fost/threading>
+
+
+using namespace fostlib;
+
+
+namespace {
+    setting< string > c_host( L"http-threaded", L"Server", L"Bind to", L"localhost" );
+    setting< int > c_port( L"http-threaded", L"Server", L"Port", 8001 );
+
+    bool service( boost::shared_ptr< http::server::request > req ) {
+        (*req)( text_body( L"<html><body>This <b>is</b> a response</body></html>", mime::mime_headers(), L"text/html" ) );
+        return true;
+    }
+}
+
+
+FSL_MAIN(
+    L"http-threaded",
+    L"Threaded HTTP server\nCopyright (c) 2009, Felspar Co. Ltd."
+)( fostlib::ostream &o, fostlib::arguments &args ) {
+    // Create a worker pool
+    workerpool pool;
+    // Bind server to host and port
+    http::server server( host( args[1].value(c_host.value()) ), c_port.value() );
+    o << L"Answering requests on http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
+    // Service requests
+    for ( bool process( true ); process; ) {
+        boost::shared_ptr< http::server::request > req( server().release() );
+        o << req->method() << L" " << req->file_spec() << " peak threads: " << pool.peak_used() << std::endl;
+        pool.f<bool>( boost::lambda::bind( service, req ) );
+    }
+    return 0;
+}
