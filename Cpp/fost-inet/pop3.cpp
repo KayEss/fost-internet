6084a191d0d1403943c5b8e302c58db5148ac124
Kirit Sælensminde <k@kirit.com>
2009-11-08 05:50:40 +0000

    Added some more changes to the email classifiers.

--- Cpp/fost-inet/pop3.cpp
+++ Cpp/fost-inet/pop3.cpp
@@ -10,6 +10,8 @@
 #include <fost/exception/out_of_range.hpp>
 #include <fost/exception/unicode_encoding.hpp>
 
+#include <boost/regex.hpp>
+
 
 using namespace fostlib;
 using namespace fostlib::pop3;
@@ -234,21 +236,26 @@ bool fostlib::pop3::email_is_an_ndr( const text_body &email ) {
         ) || (
             subject == "Delivery failure"
             || subject == "Delivery Status Notification (Failure)"
+            || subject == "Mail delivery failed: returning message to sender"
             || subject == "Undelivered Mail Returned to Sender"
         ) || (
             subject.find("DELIVERY FAILURE:") == 0
+            || subject.find("Undeliverable: ") == 0
         )
     ;
 }
 
 
+namespace {
+    const boost::regex c_delay_regex("^Warning: message delayed \\d+ .+?$");
+}
 bool fostlib::pop3::email_is_a_delay_report( const text_body &email ) {
     string subject = email.headers()["Subject"].value();
     return
         ( // Many don't have the correct report-type
             // email.headers()["Content-Type"].subvalue("report-type") == "delivery-status" &&
             subject == "Delivery Status Notification (Delay)"
-        )
+        ) || boost::regex_match( coerce< utf8_string >(subject).underlying(), c_delay_regex )
     ;
 }
 
@@ -257,9 +264,12 @@ bool fostlib::pop3::email_is_out_of_office( const text_body &email ) {
     string subject = email.headers()["Subject"].value();
     return
         (
-            subject.find("Out of Office") == 0
+            subject == "I'm out of the office"
+            || subject.find("I am out of the office") == 0
+            || subject.find(" is out of the office") != std::string::npos
+            || subject.find("Out of Office") == 0
             || subject.find("Out of Office AutoReply: ") == 0
-            || subject == "I'm out of the office"
+            || subject == "Vacation reply"
         )
     ;
 }
