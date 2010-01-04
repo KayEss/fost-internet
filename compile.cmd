ce530fd0c152ea3f400e5aafa13e65e6985375a9
Kirit Sælensminde <k@kirit.com>
2010-01-04 08:59:48 +0000

    Now compiles and builds properly on Windows.

--- compile.cmd
+++ compile.cmd
@@ -1,3 +1,19 @@
 @echo off
 echo fost-internet %*
-..\bjam preserve-test-targets=on %*
+pushd %0\..
+
+call ..\boost-version.cmd
+
+IF %BOOST_VERSION_MAJOR% LEQ 39 (
+    ..\bjam preserve-test-targets=on %*
+) ELSE (
+    ..\bjam %*
+)
+
+IF ERRORLEVEL 1 (
+    echo fost-internet %* BUILD FAIL
+    popd
+    copy
+) ELSE (
+    popd
+)
