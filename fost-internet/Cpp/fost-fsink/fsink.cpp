c2b16e0e31b1825afd04794fa80b224e6338d052
Kirit Sælensminde <k@kirit.com>
2010-11-12 13:33:46 +0000

    Added skeletons for the new logging sinks.

---
+++ fost-internet/Cpp/fost-fsink/fsink.cpp
@@ -0,0 +1,17 @@
+/*
+    Copyright 2005-2010, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/main>
+
+
+FSL_MAIN(
+    L"fsink",
+    L"fsink - Fost logging sink\nCopyright (c) 1995-2010, Felspar Co. Ltd."
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    return 0;
+}
