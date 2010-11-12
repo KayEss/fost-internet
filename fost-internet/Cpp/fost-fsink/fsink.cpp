cfe9ca3894fd845239dfb6521febe21bb065accb
Kirit Sælensminde <k@kirit.com>
2010-11-12 14:35:46 +0000

    Added a simple skeleton that should accept PUT messages from the logger.

--- fost-internet/Cpp/fost-fsink/fsink.cpp
+++ fost-internet/Cpp/fost-fsink/fsink.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2005-2010, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2010, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,11 +7,37 @@
 
 
 #include <fost/main>
+#include <fost/http>
+#include <fost/http.server.hpp>
+
+
+using namespace fostlib;
+
+
+namespace {
+    setting< string > c_host( L"fsink", L"Server", L"Bind to", L"0.0.0.0" );
+    setting< int > c_port( L"fsink", L"Server", L"Port", 23456 );
+}
 
 
 FSL_MAIN(
     L"fsink",
     L"fsink - Fost logging sink\nCopyright (c) 1995-2010, Felspar Co. Ltd."
 )( fostlib::ostream &out, fostlib::arguments &args ) {
+    http::server server( host( args[1].value(c_host.value()) ), c_port.value() );
+    out << L"Answering requests on "
+        L"http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
+    for ( bool process( true ); process; ) {
+        std::auto_ptr< http::server::request > req( server() );
+        if ( req->method() == "PUT" ) {
+            text_body response(L"Accepted", mime::mime_headers(), L"text/plain");
+            (*req)(response);
+        } else {
+            out << req->method() << L" " << req->file_spec() << std::endl;
+            text_body response(L"Message?", mime::mime_headers(), "text/plain");
+            (*req)(response);
+        }
+    }
     return 0;
 }
+
