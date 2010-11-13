88efec75b22966a58eb2e6f61041bac62a5f1c8b
Kirit Sælensminde <k@kirit.com>
2010-11-13 02:10:05 +0000

    Put together a logger that at least compiles.

--- fost-internet/Cpp/fost-netlog/http-logger.cpp
+++ fost-internet/Cpp/fost-netlog/http-logger.cpp
@@ -7,3 +7,41 @@
 
 
 #include "fost-netlog.hpp"
+#include <fost/log>
+#include <fost/http>
+
+
+using namespace fostlib;
+
+
+class http_logger {
+    http::user_agent ua;
+    public:
+        http_logger();
+        http_logger(const json &);
+
+        bool operator () (const logging::message &);
+};
+
+
+namespace {
+
+
+    const logging::global_sink< http_logger > http_put("http-put");
+
+
+}
+
+
+http_logger::http_logger() {
+}
+http_logger::http_logger(const json &) {
+}
+
+
+bool http_logger::operator () (const logging::message &) {
+    boost::shared_ptr<mime> body(new text_body(L"Some sort of body goes here"));
+    http::user_agent::request req("PUT", url("http://localhost:23456/"), body);
+    ua(req);
+    return true;
+}
