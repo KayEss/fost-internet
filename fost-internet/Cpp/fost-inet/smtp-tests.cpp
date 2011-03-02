d8956216fdeada4ab209a64b03605a12af34e163
Kirit Sælensminde <k@kirit.com>
2011-03-02 04:07:23 +0000

    Implemented a more full version of the email address parser.

--- fost-internet/Cpp/fost-inet/smtp-tests.cpp
+++ fost-internet/Cpp/fost-inet/smtp-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2011, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -16,9 +16,23 @@ using namespace fostlib;
 FSL_TEST_SUITE( smtp );
 
 
-FSL_TEST_FUNCTION( email_address ) {
+FSL_TEST_FUNCTION( email_address_without_name ) {
     fostlib::json e1("test@example.com");
     fostlib::email_address a1 = fostlib::coerce<fostlib::email_address>(e1);
     FSL_CHECK(a1.name().isnull());
     FSL_CHECK_EQ(a1.email(), "test@example.com");
+}
+
+FSL_TEST_FUNCTION( email_address_with_name ) {
+    fostlib::json e1("Test account<test@example.com>");
+    fostlib::email_address a1 = fostlib::coerce<fostlib::email_address>(e1);
+    FSL_CHECK(!a1.name().isnull());
+    FSL_CHECK_EQ(a1.name().value(), "Test account");
+    FSL_CHECK_EQ(a1.email(), "test@example.com");
+
+    fostlib::json e2("Test account <test@example.com>");
+    fostlib::email_address a2 = fostlib::coerce<fostlib::email_address>(e2);
+    FSL_CHECK(!a2.name().isnull());
+    FSL_CHECK_EQ(a2.name().value(), "Test account");
+    FSL_CHECK_EQ(a2.email(), "test@example.com");
 }
