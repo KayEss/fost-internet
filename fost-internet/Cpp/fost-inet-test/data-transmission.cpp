e7394c2cbfc79b0f7ed53668d9924a6cc83819c0
Kirit Sælensminde <k@kirit.com>
2011-07-22 07:56:41 +0000

    Added skeleton where we will do the transmission tests.

---
+++ fost-internet/Cpp/fost-inet-test/data-transmission.cpp
@@ -0,0 +1,20 @@
+/*
+    Copyright 2011, Felspar Co Ltd. http://support.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include "fost-inet-test.hpp"
+
+
+using namespace fostlib;
+
+
+FSL_TEST_SUITE( data_transmission );
+
+
+FSL_TEST_FUNCTION( large_send ) {
+    FSL_CHECK(true);
+}
