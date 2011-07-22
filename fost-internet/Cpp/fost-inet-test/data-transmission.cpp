0bfa64ccb897324282ae5f5cd5d36bd0de58e9f4
Kirit Sælensminde <k@kirit.com>
2011-07-22 08:14:11 +0000

    We now have a basic connection test we can build on.

--- fost-internet/Cpp/fost-inet-test/data-transmission.cpp
+++ fost-internet/Cpp/fost-inet-test/data-transmission.cpp
@@ -15,6 +15,20 @@ using namespace fostlib;
 FSL_TEST_SUITE( data_transmission );
 
 
+namespace {
+    void perform_hash() {
+        boost::asio::io_service service;
+        boost::asio::ip::tcp::acceptor server(
+            service, boost::asio::ip::tcp::endpoint(
+                host("0.0.0.0").address(), uint16_t(6218)));
+        std::auto_ptr< boost::asio::ip::tcp::socket > sock(
+            new boost::asio::ip::tcp::socket( service ));
+        server.accept(*sock);
+    }
+}
+
 FSL_TEST_FUNCTION( large_send ) {
-    FSL_CHECK(true);
+    worker server;
+    server(perform_hash);
+    network_connection cnx(host("localhost"), 6218);
 }
