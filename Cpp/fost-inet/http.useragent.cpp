b0958f32e4af9829c47f25275a34bdd6d0b70bc1
Kirit Sælensminde <k@kirit.com>
2014-02-22 18:08:28 +0700

    Modernise the code layout.

--- Cpp/fost-inet/http.useragent.cpp
+++ Cpp/fost-inet/http.useragent.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2012, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2008-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -30,8 +30,7 @@ namespace {
 
     const fostlib::setting< fostlib::string > c_user_agent(
         L"fost-internet/Cpp/fost-inet/http.useragent.cpp",
-        L"HTTP", L"UserAgent", L"Felspar user agent", true
-    );
+        L"HTTP", L"UserAgent", L"Felspar user agent", true);
 }
 
 
@@ -42,11 +41,11 @@ fostlib::http::user_agent::user_agent(const url &u)
 }
 
 
-std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator () (request &req) const {
+std::auto_ptr< http::user_agent::response >
+        fostlib::http::user_agent::operator () (request &req) const {
     try {
         req.headers().set("Date", coerce< string >(
-            coerce< rfc1123_timestamp >(timestamp::now())
-        ));
+            coerce< rfc1123_timestamp >(timestamp::now())));
         req.headers().set("Host", req.address().server().name());
         if ( !req.headers().exists("User-Agent") )
             req.headers().set("User-Agent", c_user_agent.value() + L"/Fost 4");
@@ -66,14 +65,16 @@ std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator 
             req.address().pathspec().underlying().underlying();
         {
             nullable< ascii_printable_string > q = req.address().query().as_string();
-            if ( !q.isnull() )
+            if ( !q.isnull() ) {
                 buffer << "?" << q.value().underlying();
+            }
         }
         buffer << " HTTP/1.0\r\n" << req.headers() << "\r\n";
         *cnx << buffer;
 
-        for ( mime::const_iterator i( req.data().begin() ); i != req.data().end(); ++i )
+        for ( mime::const_iterator i( req.data().begin() ); i != req.data().end(); ++i ) {
             *cnx << *i;
+        }
 
         utf8_string first_line;
         *cnx >> first_line;
@@ -97,16 +98,13 @@ std::auto_ptr< http::user_agent::response > fostlib::http::user_agent::operator 
                     phoenix::construct_< string >( phoenix::arg1, phoenix::arg2 ) ]
             ).full )
                 throw exceptions::not_implemented(
-                    "Expected a HTTP response", coerce< string >(first_line)
-                );
+                    "Expected a HTTP response", coerce< string >(first_line));
         }
 
         return std::auto_ptr< http::user_agent::response >(
             new http::user_agent::response(
                 cnx, req.method(), req.address(),
-                protocol, status, message
-            )
-        );
+                protocol, status, message));
     } catch ( fostlib::exceptions::exception &e ) {
         insert(e.data(), "http-ua", "method", req.method());
         throw;
@@ -177,24 +175,23 @@ boost::shared_ptr< const binary_body > fostlib::http::user_agent::response::body
     if ( !m_body ) {
         try {
             nullable< int64_t > length;
-            if ( method() == L"HEAD" )
+            if ( method() == L"HEAD" ) {
                 length = 0;
-            else if (m_headers.exists("Content-Length"))
+            } else if (m_headers.exists("Content-Length")) {
                 length = coerce< int64_t >(m_headers["Content-Length"].value());
+            }
 
-            if ( status() == 304 || (!length.isnull() && length.value() == 0) )
+            if ( status() == 304 || (!length.isnull() && length.value() == 0) ) {
                 m_body = boost::shared_ptr< binary_body >(
-                    new binary_body(m_headers)
-                );
-            else if ( length.isnull() ) {
+                    new binary_body(m_headers));
+            } else if ( length.isnull() ) {
                 if ( m_headers["Transfer-Encoding"].value() == "chunked" ) {
                     std::vector< unsigned char > data;
                     while ( true ) {
                         std::string length, ignore_crlf;
                         *m_cnx >> length;
                         std::size_t chunk_size = fostlib::coerce< std::size_t >(
-                            hex_string(length)
-                        );
+                            hex_string(length));
                         if ( chunk_size == 0 )
                             break;
                         std::vector< unsigned char > chunk( chunk_size );
@@ -202,32 +199,29 @@ boost::shared_ptr< const binary_body > fostlib::http::user_agent::response::body
                         if ( !ignore_crlf.empty() )
                             throw fostlib::exceptions::not_null(
                                 "Expected CRLF after chunk data, but found something else",
-                                coerce<string>(utf8_string(ignore_crlf))
-                            );
+                                coerce<string>(utf8_string(ignore_crlf)));
                         data.insert(data.end(), chunk.begin(), chunk.end());
                     }
                     // Read trailing headers
                     read_headers(*m_cnx, m_headers, "Whilst reading trailing headers");
                     m_body = boost::shared_ptr< binary_body >(
-                        new binary_body(data, m_headers)
-                    );
+                        new binary_body(data, m_headers));
                 } else {
                     boost::asio::streambuf body_buffer;
                     *m_cnx >> body_buffer;
                     std::vector< unsigned char > body_data;
                     body_data.reserve(body_buffer.size());
-                    while ( body_buffer.size() )
+                    while ( body_buffer.size() ) {
                         body_data.push_back( body_buffer.sbumpc() );
+                    }
                     m_body = boost::shared_ptr< binary_body >(
-                        new binary_body(body_data, m_headers)
-                    );
+                        new binary_body(body_data, m_headers));
                 }
             } else {
                 std::vector< unsigned char > body(length.value());
                 *m_cnx >> body;
                 m_body = boost::shared_ptr< binary_body >(
-                    new binary_body(body, m_headers)
-                );
+                    new binary_body(body, m_headers));
             }
         } catch ( fostlib::exceptions::exception & ) {
             throw;
