ba1a8e0657f7346fba2e0a77c29a56fb9fca2285
Kirit Sælensminde <k@kirit.com>
2014-02-27 12:01:40 +0700

    Modernise layout.

--- Cpp/fost-inet/url-tests.cpp
+++ Cpp/fost-inet/url-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2012, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 2008-2014, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -44,8 +44,7 @@ FSL_TEST_FUNCTION( query_string ) {
     FSL_CHECK( q2.as_string().isnull() );
     FSL_CHECK_EQ(
         q1.as_string().value( ascii_printable_string() ),
-        q2.as_string().value( ascii_printable_string() )
-    );
+        q2.as_string().value( ascii_printable_string() ));
     q1 = q2;
     q1.append( L"key", null );
     FSL_CHECK_EQ( q1.as_string().value(), ascii_printable_string( "key=" ) );
@@ -56,21 +55,17 @@ FSL_TEST_FUNCTION( query_string ) {
     q1.append( L"key", L"(.)" );
     FSL_CHECK_EQ(
         q1.as_string().value(),
-        ascii_printable_string( "key=&key=&key=%28.%29" )
-    );
+        ascii_printable_string( "key=&key=&key=%28.%29" ));
     FSL_CHECK_EQ(
         q2.as_string().value(),
-        ascii_printable_string( "key=&key=" )
-    );
+        ascii_printable_string( "key=&key=" ));
     q2.append( L"key", L"\x2014" );
     FSL_CHECK_EQ(
         q1.as_string().value(),
-        ascii_printable_string( "key=&key=&key=%28.%29" )
-    );
+        ascii_printable_string( "key=&key=&key=%28.%29" ));
     FSL_CHECK_EQ(
         q2.as_string().value(),
-        ascii_printable_string( "key=&key=&key=%E2%80%94" )
-    );
+        ascii_printable_string( "key=&key=&key=%E2%80%94" ));
 }
 
 
