f18edb0d7aa29e4d8831ffd858f27ee4d94fa920
Kirit Sælensminde <k@kirit.com>
2015-05-10 11:20:49 +0700

    Fix up auto_ptr.

--- Examples/webserver-simple/simple.cpp
+++ Examples/webserver-simple/simple.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008-2010 Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2015 Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -29,12 +29,11 @@ FSL_MAIN(
     o << L"Answering requests on "
         L"http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
     for ( bool process( true ); process; ) {
-        std::auto_ptr< http::server::request > req( server() );
+        std::unique_ptr<http::server::request> req(server());
         o << req->method() << L" " << req->file_spec() << std::endl;
         text_body response(
             L"<html><body>This <b>is</b> a response</body></html>",
-            mime::mime_headers(), L"text/html"
-        );
+            mime::mime_headers(), L"text/html");
         (*req)( response );
         if ( req->data()->headers()[L"Host"].value() == L"localhost" )
             process = false;
