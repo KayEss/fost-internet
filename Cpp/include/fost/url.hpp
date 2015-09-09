2686c7f368cfdac5aaa01cfbd4d9de4b5559463f
Kirit Sælensminde <k@kirit.com>
2015-09-09 11:21:39 +0700

    Fix include path.

--- Cpp/include/fost/url.hpp
+++ Cpp/include/fost/url.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 1999-2014, Felspar Co Ltd. http://support.felspar.com/
+    Copyright 1999-2015, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -13,7 +13,7 @@
 
 #include <fost/unicode>
 #include <fost/host.hpp>
-#include <fost/detail/tagged-string.hpp>
+#include <fost/tagged-string.hpp>
 
 
 namespace fostlib {
