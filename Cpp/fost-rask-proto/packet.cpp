4c0f91ff797db8796f37a4ce55d60a981f0c9f81
Kirit Sælensminde <k@kirit.com>
2017-01-06 15:20:32 +0700

    Move more of the outbound packet functionality into the right place.

--- Cpp/fost-rask-proto/packet.cpp
+++ Cpp/fost-rask-proto/packet.cpp
@@ -18,3 +18,19 @@ rask::out_packet::out_packet(out_packet &&o)
 : buffer(std::move(o.buffer)), control(o.control) {
 }
 
+
+void rask::out_packet::size_sequence(std::size_t s, boost::asio::streambuf &b) {
+    if ( s < 0x80 ) {
+        b.sputc(s);
+    } else if ( s < 0x100 ) {
+        b.sputc(0xf9);
+        b.sputc(s);
+    } else if ( s < 0x10000 ) {
+        b.sputc(0xfa);
+        b.sputc(s >> 8);
+        b.sputc(s & 0xff);
+    } else {
+        throw fostlib::exceptions::not_implemented(
+            "Large packet sizes", fostlib::coerce<fostlib::string>(s));
+    }
+}
--- Cpp/include/fost/rask/packet.hpp
+++ Cpp/include/fost/rask/packet.hpp
@@ -11,7 +11,10 @@
 
 #include <fost/rask/rask-proto.hpp>
 
+#include <fost/pointers>
+
 #include <boost/asio/streambuf.hpp>
+#include <boost/endian/conversion.hpp>
 
 
 namespace rask {
@@ -38,12 +41,63 @@ namespace rask {
         /// The control block value
         control_byte control;
 
+        /// Build a new outbound packet with the specified control byte.
         out_packet(control_byte);
 
+        /// Write a size control sequence to the specified buffer
+        static void size_sequence(std::size_t, boost::asio::streambuf &);
+
     public:
+        /// Make movable
         out_packet(out_packet &&);
+
+        /// Return the current size of the packet (minus headers)
+        std::size_t size() const {
+            return buffer->size();
+        }
+
+        /// Add the specified bytes to the buffer
+        template<typename B>
+        void bytes(fostlib::array_view<B> av) {
+            static_assert(sizeof(B), "Must add an array of bytes");
+            buffer->sputn(av.data(), av.size());
+        }
+
+        /// Add a single byte to the buffer
+        template<typename B>
+        void byte(B b) {
+            static_assert(sizeof(B), "Must add an array of bytes");
+            buffer->sputc(b);
+        }
+
+        /// Add a size control sequence
+        out_packet &size_sequence(std::size_t s) {
+            size_sequence(s, *buffer);
+            return *this;
+        }
+
+        /// Add a size control sequence for a memory block
+        out_packet &size_sequence(const fostlib::const_memory_block b) {
+            return size_sequence(
+                reinterpret_cast<const char *>(b.second) -
+                reinterpret_cast<const char *>(b.first));
+        }
     };
 
 
+}
+
+
+/// Insert an integer in network byte order
+template<typename I, typename O,
+    typename = std::enable_if_t<std::is_integral<I>::value>>
+O &operator << (O &o, I i) {
+    if ( sizeof(i) > 1 ) { // Should be constexpr if
+        auto v = boost::endian::native_to_big(i);
+        o.bytes(fostlib::array_view<char>(reinterpret_cast<char*>(&v), sizeof(v)));
+    } else {
+        o.byte(i);
+    }
+    return o;
 }
 
