9b924d3959ead48b423a61c45441bdd2a91e1721
Kirit Sælensminde <k@kirit.com>
2018-09-16 13:43:37 +0700

    Small streamlining of the `host` class.

--- Cpp/fost-inet/host.cpp
+++ Cpp/fost-inet/host.cpp
@@ -20,27 +20,20 @@ using namespace fostlib;
 namespace {
 
 
-
-    boost::asio::ip::address getaddr( const host &host ) {
-        boost::asio::io_service io_service;
-        boost::asio::ip::tcp::resolver resolver( io_service );
-        boost::asio::ip::tcp::resolver::query query(
-            coerce< ascii_string >( host.name() ).underlying(),
-            coerce< ascii_string >( host.service().value_or("0") ).underlying()
-        );
-        boost::system::error_code error;
-        boost::asio::ip::tcp::resolver::iterator it( resolver.resolve( query, error ) );
-        if ( error == boost::asio::error::host_not_found )
-            throw exceptions::host_not_found( host.name() );
-        return it->endpoint().address();
-    }
-
     fostlib::string normalise(const fostlib::string &text) {
         fostlib::string ret;
         for ( fostlib::string::const_iterator c(text.begin()); c != text.end(); ++c )
             ret += std::tolower(*c);
         return ret;
     }
+
+
+    fostlib::string ipname(uint32_t ipv4) {
+        return coerce<string>(
+            boost::asio::ip::address{
+                boost::asio::ip::address_v4{ipv4}});
+    }
+
 
 }
 
@@ -59,20 +52,24 @@ fostlib::host::host( const fostlib::string &name, port_number service )
 }
 
 fostlib::host::host( uint32_t address, const nullable< string > &service )
-: service( service ), m_address( boost::asio::ip::address_v4( address ) ) {
-    m_name = coerce< string >( m_address.value() );
+: service(service), m_name(ipname(address)) {
 }
 fostlib::host::host( uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, const nullable< string > &service )
-: service( service ), m_address(
-    boost::asio::ip::address_v4( ( b1 << 24 ) + ( b2 << 16 ) + ( b3 << 8 ) + b4 )
-) {
-    m_name = coerce< string >( m_address.value() );
+: service(service), m_name(ipname((b1 << 24) + (b2 << 16) + (b3 << 8) + b4 )) {
 }
 
 
 boost::asio::ip::address fostlib::host::address() const {
-    if ( not m_address ) m_address = getaddr( *this );
-    return m_address.value();
+    boost::asio::io_service io_service;
+    boost::asio::ip::tcp::resolver resolver( io_service );
+    boost::asio::ip::tcp::resolver::query query(
+        coerce<ascii_string>(name()).underlying(),
+        coerce<ascii_string>(service().value_or("0")).underlying());
+    boost::system::error_code error;
+    boost::asio::ip::tcp::resolver::iterator it{resolver.resolve(query, error)};
+    if ( error == boost::asio::error::host_not_found )
+        throw exceptions::host_not_found(name());
+    return it->endpoint().address();
 }
 
 
