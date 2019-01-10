448bff0fed0d24eeb9105e4a1506afe62640c634
Kirit Sælensminde <k@kirit.com>
2019-01-10 09:01:54 +0700

    This check must be on the byte lengths.

--- Cpp/fost-inet/mime-coercion.cpp
+++ Cpp/fost-inet/mime-coercion.cpp
@@ -1,13 +1,14 @@
-/*
-    Copyright 2011, Felspar Co Ltd. http://support.felspar.com/
+/**
+    Copyright 2011-2019, Felspar Co Ltd. <http://support.felspar.com/>
+
     Distributed under the Boost Software License, Version 1.0.
-    See accompanying file LICENSE_1_0.txt or copy at
-        http://www.boost.org/LICENSE_1_0.txt
+    See <http://www.boost.org/LICENSE_1_0.txt>
 */
 
 
 #include "fost-inet.hpp"
 #include <fost/mime.hpp>
+#include <fost/insert>
 
 
 fostlib::utf8_string
@@ -17,9 +18,13 @@ fostlib::utf8_string
     for (fostlib::mime::const_iterator i(input.begin()); i != input.end();
          ++i) {
         fostlib::utf8_string data(fostlib::coerce<fostlib::utf8_string>(*i));
-        if (data.underlying().length() < bytes(*i))
-            throw fostlib::exceptions::not_implemented(
-                    "Not all data converted");
+        if (data.underlying().bytes() < bytes(*i)) {
+            fostlib::exceptions::not_implemented err{
+                __PRETTY_FUNCTION__, "Not all data converted"};
+            fostlib::insert(err.data(), "data", "bytes", data.underlying().bytes());
+            fostlib::insert(err.data(), "bytes", bytes(*i));
+            throw err;
+        }
         string += data;
     }
     return string;
