d67affec0b76c5a3332e3d42da5a862fa44f4760
Kirit Sælensminde <kirit@kirit.com>
2020-04-23 12:04:17 +0700

    Will need more tests here.

---
+++ Cpp/fost-inet-test/http.cpp
@@ -0,0 +1,16 @@
+/**
+    Copyright 2020 Red Anchor Trading Co. Ltd.
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+ */
+
+
+#include "fost-inet-test.hpp"
+#include <fost/test>
+
+
+FSL_TEST_SUITE(http);
+
+
+FSL_TEST_FUNCTION(basic_request) {}
