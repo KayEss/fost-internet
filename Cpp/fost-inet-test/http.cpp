0b70cf4cd4b4b5bb22421c51b3923fb84f250e8d
Kirit Sælensminde <kirit@kirit.com>
2020-04-23 15:43:59 +0700

    Fix synchronisation in the test so we don't need the sleep.

--- Cpp/fost-inet-test/http.cpp
+++ Cpp/fost-inet-test/http.cpp
@@ -20,14 +20,13 @@ FSL_TEST_SUITE(http);
 
 
 FSL_TEST_FUNCTION(basic_request) {
-    std::thread{[]() {
-        fostlib::http::server server{{127, 0, 0, 1}, 45634};
+    fostlib::http::server server{{127, 0, 0, 1}, 45634};
+    std::thread{[&server]() {
         auto req = server();
         fostlib::text_body response{"OK"};
         (*req)(response);
     }}.detach();
     fostlib::http::user_agent ua{{}};
-    std::this_thread::sleep_for(100ms); // TODO use some proper synchronization
     auto const response = ua.get(fostlib::url{"http://127.0.0.1:45634"});
     FSL_CHECK_EQ(response->status(), 200);
 }
