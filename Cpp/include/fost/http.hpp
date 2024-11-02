481a94cf5df39b07d6dffa2dc119745a413634f9
Kirit Sælensminde <kirit@felspar.com>
2024-11-02 14:34:11 +0700

    Remove dead comment

--- Cpp/include/fost/http.hpp
+++ Cpp/include/fost/http.hpp
@@ -1,11 +1,3 @@
-/**
-    Copyright 2008-2019 Red Anchor Trading Co. Ltd.
-
-    Distributed under the Boost Software License, Version 1.0.
-    See <http://www.boost.org/LICENSE_1_0.txt>
- */
-
-
 #ifndef FOST_INTERNET_HTTP_HPP
 #define FOST_INTERNET_HTTP_HPP
 #pragma once
